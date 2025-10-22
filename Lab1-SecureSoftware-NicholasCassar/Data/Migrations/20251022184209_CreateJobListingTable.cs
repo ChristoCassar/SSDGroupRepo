79f0fd0fcae14a46100cadc10b3a8835a0ad2de0
Robert Kumar <rob_kumar@outlook.com>
2025-10-22 15:06:39 -0400

    Add migrations for group

---
+++ Lab1-SecureSoftware-NicholasCassar/Data/Migrations/20251022184209_CreateJobListingTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab1_SecureSoftware_NicholasCassar.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateJobListingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobListing",
+                columns: table => new
+                {
+                    JobId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    JobAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Urgency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobListing", x => x.JobId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobListing");
+        }
+    }
+}
